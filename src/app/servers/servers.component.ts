e6c60a7a3192a0b74458e70490b06f89b553beea
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-03-21 23:01:44 -0700

    Show different selector options in the servers.component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+   // selector: '[app-servers]',    css attribute (<div app-servers></div>)
+   // selector: '.app-servers',     css class (<div class="app-servers"></div>)
   selector: 'app-servers',
   template: `
    <app-server></app-server>
